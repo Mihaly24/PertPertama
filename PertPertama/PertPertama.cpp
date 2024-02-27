785942bdc969e11b60e6ebd427b13a04327f70e4
Mihaly24 <fairuzk20@gmail.com>
2024-02-27 19:06:01 +0700

    Menambahkan header

--- PertPertama/PertPertama.cpp
+++ PertPertama/PertPertama.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
 	float nJejari, nLuas;
+	cout << "======Luas Lingkaran======" << endl;
 		cout << "Masukkan jejari= ";
 		cin >> nJejari;
 		nLuas = 3.14 * nJejari * nJejari;
